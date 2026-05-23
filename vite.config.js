19586283ecb27e6689c938b84b864fbec5f0336d
AlanChrist-A <alanchrist@student.tce.edu>
2026-05-23 17:19:06 +0530

    Prepare site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Team-Buffer/",
   plugins: [react()],
-  server: {
-    port: 8080,
-  },
+  server: { port: 8080 },
 });
